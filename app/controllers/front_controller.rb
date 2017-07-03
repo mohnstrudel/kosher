cf4f9668861a542b5dbcb8a98d0e21ad934bd628
Anton Kostin <schnibba@gmail.com>
2017-07-03 13:48:11 +0300

    Before action instead of before filter.

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -1,7 +1,7 @@
 class FrontController < ApplicationController
   layout 'front' 
 
-  before_filter :force_blank_request_format_to_html
+  before_action :force_blank_request_format_to_html
 
   rescue_from ActionView::MissingTemplate do |exception|
     logger.debug "Missing template detected for path:"
