95d23a2cc4e598fcc0829ad0e9b817c2f4e56cf6
Anton Kostin <schnibba@gmail.com>
2017-07-03 13:41:57 +0300

    Add missing template rescue and default renderer.

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -1,3 +1,20 @@
 class FrontController < ApplicationController
-  layout 'front'  
+  layout 'front' 
+
+  before_filter :force_blank_request_format_to_html
+
+  rescue_from ActionView::MissingTemplate do |exception|
+    logger.debug "Missing template detected for path:"
+    logger.debug exception.path
+    # use exception.path to extract the path information
+    # This does not work for partials
+  end
+
+  private
+
+  def force_blank_request_format_to_html
+    if request[:format].blank? 
+      request.format = :html 
+    end
+  end 
 end
