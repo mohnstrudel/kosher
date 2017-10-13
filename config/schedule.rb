f30c5b7ba11783688d95501a9cfb8d0945cc77ed
Anton Kostin <schnibba@gmail.com>
2017-10-13 14:18:09 +0300

    Rake job for sitemap.xml.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,8 @@
+env :PATH, ENV['PATH']
+
+every 1.day, at: '5:00 am' do
+  rake "-s sitemap:refresh"
+end
 # Use this file to easily define all of your cron jobs.
 #
 # It's helpful, but not entirely necessary to understand cron before proceeding.
