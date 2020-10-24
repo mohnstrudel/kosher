650afe8499271d30421226fc71a331748659b421
Anton Kostin <anton.kostin@mhm-hr.com>
2020-10-24 16:45:10 +0200

    KOSHER-007: Поиск только по точному слову. Сортировка выдачи поиска - сначала продукты, потом производители.

--- app/controllers/front/main_searches_controller.rb
+++ app/controllers/front/main_searches_controller.rb
@@ -1,5 +1,8 @@
 class Front::MainSearchesController < FrontController
   def show
-    @results = PgSearch.multisearch(params[:main_search][:query])
+    @results = PgSearch.multisearch(params[:main_search][:query]).to_a # convert to an array to enable sorting and grouping
+    sorting_order = ["Product", "Manufacturer"]
+
+    @results = @results.sort_by { |result| sorting_order.index result.searchable_type }
   end
 end
--- config/initializers/pg_search.rb
+++ config/initializers/pg_search.rb
@@ -1,3 +1,4 @@
 PgSearch.multisearch_options = {
-  using: {tsearch: {dictionary: "russian", prefix: true}}
+  # using: {tsearch: {dictionary: "russian", prefix: true}}
+  using: {tsearch: {dictionary: "simple", prefix: true}}
 }
